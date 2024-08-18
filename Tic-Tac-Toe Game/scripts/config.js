fe34e4a7c69fbc30da22689e76f8d3bd72e4c2a4
Mandula Thrimanne <thrimanne@gmail.com>
2024-08-18 15:32:53 -0400

    JavaScript-321

--- Tic-Tac-Toe Game/scripts/config.js
+++ Tic-Tac-Toe Game/scripts/config.js
@@ -1,4 +1,5 @@
-function openPlayerConfig() {
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid; // +'1' --> 1
   playerConfigOverlayElement.style.display = "block";
   backDropElement.style.display = "block";
 }
@@ -8,6 +9,7 @@ function closePlayerConfig() {
   backDropElement.style.display = "none";
   formElement.firstElementChild.classList.remove("error");
   errorOutput.textContent = "";
+  formElement.firstElementChild.lastElementChild.value = "";
 }
 
 function savePlayerConfig(event) {
@@ -15,9 +17,18 @@ function savePlayerConfig(event) {
   const formData = new FormData(event.target);
   const enteredPlayerName = formData.get("playername").trim();
 
-  if (!enteredPlayerName)
+  if (!enteredPlayerName) {
     // enteredPlayerName === ''
     event.target.firstElementChild.classList.add("error");
-  errorOutput.textContent = "Please enter a valid name!";
-  return;
+    errorOutput.textContent = "Please enter a valid name!";
+    return;
+  }
+  const updatedPLayerDataElement = document.getElementById(
+    "player-" + editedPlayer + "-data"
+  );
+  updatedPLayerDataElement.children[1].textContent = enteredPlayerName;
+
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
 }
